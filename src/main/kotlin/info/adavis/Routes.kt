ad6d65b9c8833b33ae5c27ddcdd66a8426918e6c
Annyce Davis <annyce.davis@gmail.com>
2019-12-27 17:08:11 -0500

    [*] Swapped from static default to file

--- src/main/kotlin/info/adavis/Routes.kt
+++ src/main/kotlin/info/adavis/Routes.kt
@@ -3,11 +3,16 @@ package info.adavis
 import com.google.gson.Gson
 import info.adavis.graphql.AppSchema
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.log
-import io.ktor.http.content.default
+import io.ktor.http.content.file
 import io.ktor.http.content.static
+import io.ktor.http.content.staticRootFolder
+import io.ktor.response.respondRedirect
+import io.ktor.routing.get
 import io.ktor.routing.routing
 import org.koin.ktor.ext.inject
+import java.io.File
 
 @Suppress("unused")
 fun Application.routes() {
@@ -17,8 +22,13 @@ fun Application.routes() {
 
         graphql(log, gson, appSchema.schema)
 
-        static("/") {
-            default("index.html")
+        static {
+            staticRootFolder = File("src/main/static")
+            file("index.html")
+        }
+
+        get("/") {
+            call.respondRedirect("index.html")
         }
     }
 }
